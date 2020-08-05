c403822418bfdc3c081d53d776d6885eec1d7a70
Nathan Steed <steedn@outlook.com>
2020-08-05 21:34:38 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,38 @@
+# class Book
+#   attr_accessor :author, :page_count
+#   attr_reader :title, :genre
+#
+#     GENRE = []
+#
+#   def initialize(title)
+#     @title = title
+#   end
+#
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+#   def genre=(genre)
+#     @genre = genre
+#     GENRE << genre
+#   end
+# end
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+attr_accessor :author, :page_count  # remove the attr_accessor for genre
+attr_reader :title, :genre  # add an attr_reader for genre
 
-  def initialize(title)
-    @title = title
-  end
+GENRES = []
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+def initialize(title)
+  @title = title
+end
 
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+
+# create the writer for genre and add the logic for the class constant
+def genre=(genre)
+  @genre = genre
+  GENRES << genre
+end
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,36 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
+
+# attr_accessor :author, :page_count  # remove the attr_accessor for genre
+# attr_reader :title, :genre  # add an attr_reader for genre
+#
+# GENRES = []
+#
+# def initialize(title)
+#   @title = title
+# end
+#
+# def turn_page
+#   puts "Flipping the page...wow, you read fast!"
+# end
+#
+# # create the writer for genre and add the logic for the class constant
+# def brand=(brand)
+#   @genre = genre
+#   GENRES << genre
+# end
+# end
