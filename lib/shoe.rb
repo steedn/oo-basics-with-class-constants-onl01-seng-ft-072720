973dcf1452d9397934bcf309c0d64aaf4e83d906
Nathan Steed <steedn@outlook.com>
2020-08-05 20:23:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -20,3 +20,23 @@ class Shoe
     binding.pry
   end
 end
+
+# attr_accessor :author, :page_count  # remove the attr_accessor for genre
+# attr_reader :title, :genre  # add an attr_reader for genre
+#
+# GENRES = []
+#
+# def initialize(title)
+#   @title = title
+# end
+#
+# def turn_page
+#   puts "Flipping the page...wow, you read fast!"
+# end
+#
+# # create the writer for genre and add the logic for the class constant
+# def brand=(brand)
+#   @genre = genre
+#   GENRES << genre
+# end
+# end
