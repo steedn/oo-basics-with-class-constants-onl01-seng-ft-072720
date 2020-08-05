1c14d90e7c59961885dd2ed01b75ce79a24040c8
Nathan Steed <steedn@outlook.com>
2020-08-05 19:11:30 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,18 +1,37 @@
-class Book
-  attr_accessor :author, :page_count
-  attr_reader :title, :genre
+# class Book
+#   attr_accessor :author, :page_count
+#   attr_reader :title, :genre
+# 
+#     GENRE = []
+# 
+#   def initialize(title)
+#     @title = title
+#   end
+# 
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+#   def genre=(genre)
+#     @genre = genre
+#     GENRE << genre
+#   end
+# end
+attr_accessor :author, :page_count  # remove the attr_accessor for genre
+attr_reader :title, :genre  # add an attr_reader for genre
 
-    GENRE = []
+GENRES = []
 
-  def initialize(title)
-    @title = title
-  end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-  def genre=(genre)
-    @genre = genre
-    GENRE << genre
-  end
+def initialize(title)
+  @title = title
 end
+
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+
+# create the writer for genre and add the logic for the class constant
+def genre=(genre)
+  @genre = genre
+  GENRES << genre 
+end
+en
