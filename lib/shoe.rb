9248c4f90037bdadbfeae7c1ae01b3c2c15790cb
Nathan Steed <steedn@outlook.com>
2020-08-05 19:15:36 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,8 +13,8 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
   def brand=(brand)
-    @brand = brand
-    BRANDS << brand
+    BRANDS << @brand
   end
 end
